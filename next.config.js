7f27aacebaed39eea3408df0ff373a5e111be7c3
mina <teenage.web.academy@gmail.com>
2022-12-19 00:57:02 +0330

    image path 10

--- next.config.js
+++ next.config.js
@@ -1,39 +1,39 @@
-// const isGithubActions = process.env.GITHUB_ACTIONS || false
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-// let assetPrefix = ''
-// let basePath = '/'
+let assetPrefix = ''
+let basePath = '/'
 
-// if (isGithubActions) {
-//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-//   assetPrefix = `/${repo}/`
-//   basePath = `/${repo}`
-// }
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
-// module.exports = {
-//   assetPrefix: assetPrefix,
-//   basePath: basePath,
-//   images: {
-//     loader: 'imgix',
-//     path: 'the "domain" of your Imigix source',
-//   },
-// }
-
-
-
-
-/**   @type {import('next').NextConfig}*/
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
-    loader: 'akamai',
-    path: 'https://teenagewebacademy.github.io/introduction/',
+    loader: 'imgix',
+    path: 'https://teenagewebacademy.github.io/introduction/public',
   },
-  basePath: '/introduction',
-  assetPrefix: '/introduction',
-};
-module.exports = nextConfig
+}
+
+
+
+
+// /**   @type {import('next').NextConfig}*/
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     loader: 'akamai',
+//     path: 'https://teenagewebacademy.github.io/introduction/',
+//   },
+//   basePath: '/introduction',
+//   assetPrefix: '/introduction',
+// };
+// module.exports = nextConfig
 
 
 
