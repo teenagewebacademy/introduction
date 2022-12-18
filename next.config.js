d3f45e408c5f635543546f45850bfd2d0d9ba4cb
mina <teenage.web.academy@gmail.com>
2022-12-19 00:53:30 +0330

    image path 9

--- next.config.js
+++ next.config.js
@@ -1,16 +1,38 @@
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+// let assetPrefix = ''
+// let basePath = '/'
+
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
+
+// module.exports = {
+//   assetPrefix: assetPrefix,
+//   basePath: basePath,
+//   images: {
+//     loader: 'imgix',
+//     path: 'the "domain" of your Imigix source',
+//   },
+// }
+
+
+
 
 /**   @type {import('next').NextConfig}*/
-
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     loader: 'akamai',
-    path: './',
+    path: 'https://teenagewebacademy.github.io/introduction/',
   },
   basePath: '/introduction',
   assetPrefix: '/introduction',
 };
-
 module.exports = nextConfig
 
 
