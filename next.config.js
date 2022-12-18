945f76f8a79bd732c9f269ea17e6c9b2a7a9835a
mina <teenage.web.academy@gmail.com>
2022-12-18 21:52:17 +0330

    change next.config

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
     loader: 'akamai',
     path: '',
   },
-  // assetPrefix: '/nextjs-pages',
-  // basePath: '/nextjs-pages'
+  basePath: '/nextjs-pages',
+  assetPrefix: '/nextjs-pages',
 };
 
 module.exports = nextConfig
