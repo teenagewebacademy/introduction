7d89560dbdfbf45f7f80d2f52cb7574655ff0afc
mina <teenage.web.academy@gmail.com>
2022-12-18 22:57:27 +0330

    image path4

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'akamai',
-    path: '/public',
+    basePath: '/public',
   },
   basePath: '/introduction',
   assetPrefix: '/introduction',
