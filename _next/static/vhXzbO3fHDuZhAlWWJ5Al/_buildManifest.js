b6f83a5790954b598acb6f5f8cde380317bc6648
teenagewebacademy <113044205+teenagewebacademy@users.noreply.github.com>
2022-12-18 20:27:10 +0000

    Deploying to gh-pages from @ teenagewebacademy/introduction@a99d4de9ba4c122145aea46fba1c26ce52452048 🚀

---
+++ _next/static/vhXzbO3fHDuZhAlWWJ5Al/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/902-35e553370b23467b.js","static/css/c32fac97bb1e012e.css","static/chunks/pages/index-7fe122334a20072e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
