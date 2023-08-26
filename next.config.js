5a47a293eec8a9d5ba4a6eac2ff18eea6a7bdd80
teenagewebacademy <teenage.web.academy@gmail.com>
2023-08-26 15:07:12 +0330

    1-new term project

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 
-module.exports = {
-  assetPrefix: '/introduction',  
-  basePath: '/introduction',
-  images: {
-    loader: 'akamai',  
-    path: '',
-  },
-}
-
-
-
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   /* config options here */
+// module.exports = {
+//   assetPrefix: '/introduction',  
+//   basePath: '/introduction',
+//   images: {
+//     loader: 'akamai',  
+//     path: '',
+//   },
 // }
+
+
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+}
  
-// module.exports = nextConfig
+module.exports = nextConfig
