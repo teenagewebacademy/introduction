2dc4941dcb33454dc7b493eea9fe443edf4371ae
teenagewebacademy <teenage.web.academy@gmail.com>
2023-10-01 01:06:15 +0330

    update confiig

--- next.config.js
+++ next.config.js
@@ -6,20 +6,12 @@ module.exports = {
     path: '',
   },
 }
-<<<<<<< HEAD
 
 
-
-
-=======
->>>>>>> 72577cf8bd20aaec86200626acc77bfabd9116cf
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   /* config options here */
 // }
  
 // module.exports = nextConfig
-<<<<<<< HEAD
 
-=======
->>>>>>> 72577cf8bd20aaec86200626acc77bfabd9116cf
