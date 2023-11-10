628828aa4e7c5b2512384d93da5bda2edec6abc5
teenagewebacademy <teenage.web.academy@gmail.com>
2023-11-10 16:52:29 +0330

    update mft & term 1-3

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-// module.exports = {
-//   assetPrefix: '/introduction',  
-//   basePath: '/introduction',
-//   images: {
-//     loader: 'akamai',  
-//     path: '',
-//   },
-// }
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
+module.exports = {
+  assetPrefix: '/introduction',  
+  basePath: '/introduction',
+  images: {
+    loader: 'akamai',  
+    path: '',
+  },
 }
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// }
  
-module.exports = nextConfig
+// module.exports = nextConfig
 
