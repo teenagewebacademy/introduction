9710c57478d6dd2e9bfd42c58fbd98ba507edb3d
teenagewebacademy <teenage.web.academy@gmail.com>
2023-11-16 13:57:10 +0330

    maede & alireza project again

--- pages/Project.js
+++ pages/Project.js
@@ -2,7 +2,6 @@
 
 
 import React, { useState, useEffect } from 'react'
-import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Project.module.css'
 import { useRouter } from 'next/router'
