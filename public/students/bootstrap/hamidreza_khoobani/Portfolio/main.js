acf1dd93f1b019701135e210aea68e5b775d5c6b
teenagewebacademy <teenage.web.academy@gmail.com>
2024-06-13 22:17:43 +0330

    web2

---
+++ public/students/bootstrap/hamidreza_khoobani/Portfolio/main.js
@@ -0,0 +1,29 @@
+// let name1='hamid',name2='reza',name3='khobani'
+
+// console.log(name1+'  '+name2+'  '+name3);
+
+// let name='hamid'
+// let age='18'
+
+// console.log(name + ' is : ' + age);
+// console.log(`my name is ${name} & ${age} years old`);
+// console.log("my friend's are :\n amir \n hossein \n reza \n and,....");
+
+// console.log(5+10+15);
+// console.log(2+15+'20');
+// console.log(10+'20'+30);
+// console.log('10'+10+20);
+
+let name4='   hamid  reza ';
+// let name5=new String('reza');
+// let andazah=name4.length;
+// console.log(andazah);
+
+console.log(name4.toUpperCase());
+console.log(name4.toLowerCase());
+console.log(name4.trim());
+console.log(name4.trimStart());
+console.log(name4.trimEnd());
+console.log(name4.includes(' '));
+console.log(name4.split(" "));
+
